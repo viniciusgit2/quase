fdc42c4e977e1063e45620447bccaef8300d855f
viniciusgit2 <4xv6patelfin@gmail.com>
2022-08-12 11:55:40 -0300

    ver o que tem de errado

---
+++ src/pages/id/buscar.jsx
@@ -0,0 +1,5 @@
+export default  function  buscar(){
+return (
+    <div><h1>rotas id buscar</h1></div>
+)
+}
---
+++ src/pages/id/navegador.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import buscar from "./buscar";
+export default function navegador (){
+    return (
+        <div> <h1>rotas</h1>
+        
+        <button>rediricionar</button>
+        <Link  href="./buscar"></Link>
+        <li>rota1</li>
+            </div>
+            
+    )
+}
